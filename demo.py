3f2004545b097691a125bb6ee2bdcaf56a8dee55
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-26 21:26:56 +0800

    add KC705 demo

---
+++ demo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from migen import *
+from migen.build.platforms import kc705
+from migen.genlib.io import CRG
+
+from microscope import *
+
+
+class MicroscopeDemo(Module):
+    def __init__(self, serial_pads, sys_clk_freq):
+        counter = Signal(32)
+        toggle = Signal()
+        self.comb += toggle.eq(counter[29])
+        self.sync += counter.eq(counter + 1)
+
+        add_probe_single("demo", "toggle", toggle)
+        add_probe_buffer("demo", "counter", counter)
+
+        self.submodules += Microscope(serial_pads, sys_clk_freq)
+
+
+def main():
+    platform = kc705.Platform()
+    top = MicroscopeDemo(platform.request("serial"), 1e9/platform.default_clk_period)
+    clock = platform.request(platform.default_clk_name)
+    top.submodules += CRG(clock)
+    platform.build(top)
+
+
+if __name__ == "__main__":
+    main()

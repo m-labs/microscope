92c3b22eb6f1d0b1c6848ced81b680d9d7edd56c
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-27 22:49:57 +0800

    add packaging boilerplate

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+
+if sys.version_info[:3] < (3, 5):
+    raise SystemExit("You need Python 3.5+")
+
+
+setup(
+    name="microscope",
+    version="1.0",
+    description="A simple FPGA logic analyzer for Migen designs",
+    author="Sebastien Bourdeauducq",
+    author_email="sb@m-labs.hk",
+    url="https://m-labs.hk",
+    download_url="https://github.com/m-labs/microscope",
+    license="BSD",
+    platforms=["Any"],
+    keywords="HDL ASIC FPGA hardware design",
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+        "Environment :: Console",
+        "Development Status :: Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    packages=find_packages(),
+    install_requires=["migen", "pyserial", "msgpack-python", "prettytable"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["microscope = microscope.microscope:main"],
+    },
+)

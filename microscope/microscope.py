5cfb0568631ad7c788c56894cf5b217c8ccdcf35
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-27 21:05:31 +0800

    implement minimal feature set in host communication program

--- microscope/microscope.py
+++ microscope/microscope.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import serial
 import msgpack
 import struct
+import prettytable
 
 
 class Comm:
@@ -17,7 +19,7 @@ class Comm:
 
     def get_config(self):
         self.ser.write(Comm.magic + b"\x00")
-        return next(msgpack.Unpacker(self.ser, read_size=1))
+        return next(msgpack.Unpacker(self.ser, read_size=1, encoding="utf-8"))
 
     def select(self, insert):
         self.ser.write(Comm.magic + b"\x01" + struct.pack("B", insert))
@@ -27,21 +29,89 @@ class Comm:
 
     def pending(self):
         self.ser.write(Comm.magic + b"\x03")
-        return struct.unpack("?", ser.read(1))[0]
+        return struct.unpack("?", self.ser.read(1))[0]
 
-    def data(self, len):
+    def data(self, length):
         self.ser.write(Comm.magic + b"\x04")
-        return ser.read(len)
+        return self.ser.read(length)
+
+
+def display_inserts(comm):
+    config = comm.get_config()
+    table = prettytable.PrettyTable(["Group", "Name", "Width", "Depth"])
+    for group, name, width, depth in config["ins"]:
+        group = config["grp"][group]
+        table.add_row([group, name, width, depth])
+    print(table)
+
+
+def display_singles(comm):
+    config = comm.get_config()
+    table = prettytable.PrettyTable(["Group", "Name", "Value"])
+    for i, (group, name, width, depth) in enumerate(config["ins"]):
+        if depth == 1:
+            comm.select(i)
+            comm.arm()
+            while comm.pending():
+                pass
+            data = comm.data((width+7)//8)
+            value = int.from_bytes(data, "little")
+            group = config["grp"][group]
+            table.add_row([group, name, hex(value)])
+    print(table)
+
+
+def display_buffer(comm, q_group, q_name, q_n):
+    config = comm.get_config()
+    try:
+        q_group = config["grp"].index(q_group)
+    except IndexError:
+        raise SystemExit("Group not found")
+    found = False
+    n = 0
+    for i, (group, name, width, depth) in enumerate(config["ins"]):
+        if group == q_group and name == q_name:
+            if q_n is None or n == q_n:
+                found = True
+                comm.select(i)
+                comm.arm()
+                print("waiting for trigger...", file=sys.stderr)
+                while comm.pending():
+                    pass
+                print("done", file=sys.stderr)
+
+                word_len = (width+7)//8
+                data = comm.data(depth*word_len)
+                print("[")
+                for j in range(depth):
+                    print(hex(int.from_bytes(data[j*word_len:(j+1)*word_len], "little")) + ",")
+                print("]")
+            n += 1
+    if not found:
+        raise SystemExit("Insert not found")
 
 
 def main():
     parser = argparse.ArgumentParser(description="Microscope FPGA logic analyzer client")
-    parser.add_argument("port", help="serial port")
+    parser.add_argument("port", help="serial port URL (see open_for_url in pyserial)")
+    subparsers = parser.add_subparsers(dest="action")
+    subparsers.add_parser("inserts", help="list inserts available on the target device")
+    subparsers.add_parser("singles", help="show current values of single-value inserts")
+    parser_buffer = subparsers.add_parser("buffer", help="show values of a buffering insert")
+    parser_buffer.add_argument("group", metavar="GROUP")
+    parser_buffer.add_argument("name", metavar="NAME")
+    parser_buffer.add_argument("-n", type=int, default=None,
+                               help="index (in case of multiple matches)")
     args = parser.parse_args()
 
     comm = Comm(args.port)
     try:
-        print(comm.get_config())
+        if args.action is None or args.action == "inserts":
+            display_inserts(comm)
+        elif args.action == "singles":
+            display_singles(comm)
+        elif args.action == "buffer":
+            display_buffer(comm, args.group, args.name, args.n)
     finally:
         comm.close()
 

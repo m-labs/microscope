6320503525a283e2277cca542a59a95af09616ee
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-28 16:41:08 +0800

    import add_probe_async by default

--- microscope/__init__.py
+++ microscope/__init__.py
@@ -1,2 +1,2 @@
-from microscope.globals import add_probe_single, add_probe_buffer
+from microscope.globals import add_probe_async, add_probe_single, add_probe_buffer
 from microscope.core import Microscope

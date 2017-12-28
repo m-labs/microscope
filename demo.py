bf14ecc118db5008c54e078b301b933cb955b91b
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-28 18:27:49 +0800

    demo: add another single probe

--- demo.py
+++ demo.py
@@ -10,12 +10,19 @@ from microscope import *
 class MicroscopeDemo(Module):
     def __init__(self, serial_pads, sys_clk_freq):
         counter = Signal(32)
-        toggle = Signal()
-        self.comb += toggle.eq(counter[29])
+        toggle1 = Signal()
+        toggle2 = Signal()
+        self.comb += [
+            toggle1.eq(counter[29]),
+            toggle2.eq(counter[28])
+        ]
         self.sync += counter.eq(counter + 1)
 
-        self.submodules += add_probe_single("demo", "toggle", toggle)
-        self.submodules += add_probe_buffer("demo", "counter", counter)
+        self.submodules += [
+            add_probe_single("demo", "toggle1", toggle1),
+            add_probe_single("demo", "toggle2", toggle2),
+            add_probe_buffer("demo", "counter", counter)
+        ]
 
         self.submodules += Microscope(serial_pads, sys_clk_freq)
 

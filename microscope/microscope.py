203ad6b7db33d8ffb1565c2b330ce209c21afef2
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-12-30 00:59:06 +0800

    add monitor feature

--- microscope/microscope.py
+++ microscope/microscope.py
@@ -62,6 +62,38 @@ def display_singles(comm):
     print(table)
 
 
+def monitor_single(comm, q_group, q_name, q_n):
+    config = comm.get_config()
+    try:
+        q_group = config["grp"].index(q_group)
+    except IndexError:
+        raise SystemExit("Group not found")
+    found = None
+    n = 0
+    for i, (group, name, width, depth) in enumerate(config["ins"]):
+        if group == q_group and name == q_name and depth == 1:
+            if q_n is None or n == q_n:
+                if found is not None:
+                    raise SystemExit("More than one insert matches")
+                found = i
+    if found is None:
+        raise SystemExit("Insert not found")
+
+    _, _, width, _ = config["ins"][found]
+    fmtstring = "{:0" + str((width+3)//4) + "x}"
+    toggle = False
+    comm.select(found)
+    while True:
+        comm.arm()
+        while comm.pending():
+            pass
+        data = comm.data((width+7)//8)
+        value = int.from_bytes(data, "little")
+        print(("/ " if toggle else "\\ ") + fmtstring.format(value),
+              end="\r", flush=True)
+        toggle = not toggle
+
+
 def display_buffer(comm, q_group, q_name, q_n):
     config = comm.get_config()
     try:
@@ -98,6 +130,11 @@ def main():
     subparsers = parser.add_subparsers(dest="action")
     subparsers.add_parser("inserts", help="list inserts available on the target device")
     subparsers.add_parser("singles", help="show current values of single-value inserts")
+    parser_monitor = subparsers.add_parser("monitor", help="continously monitor the value of a single-value insert")
+    parser_monitor.add_argument("group", metavar="GROUP")
+    parser_monitor.add_argument("name", metavar="NAME")
+    parser_monitor.add_argument("-n", type=int, default=None,
+                                help="index (in case of multiple matches)")
     parser_buffer = subparsers.add_parser("buffer", help="show values of a buffering insert")
     parser_buffer.add_argument("group", metavar="GROUP")
     parser_buffer.add_argument("name", metavar="NAME")
@@ -111,6 +148,8 @@ def main():
             display_inserts(comm)
         elif args.action == "singles":
             display_singles(comm)
+        elif args.action == "monitor":
+            monitor_single(comm, args.group, args.name, args.n)
         elif args.action == "buffer":
             display_buffer(comm, args.group, args.name, args.n)
     finally:
